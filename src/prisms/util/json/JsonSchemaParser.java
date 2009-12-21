08ac18e4d276bfe8095fed63e1fadc724d244086
Andrew Butler <Updownquark@gmail.com>
2009-12-21 20:21:27 +0000

    Added the parser's ability to look up other schemas in .json files.

--- src/prisms/util/json/JsonSchemaParser.java
+++ src/prisms/util/json/JsonSchemaParser.java
@@ -16,6 +16,21 @@ public class JsonSchemaParser
 	 */
 	public static final Logger log = Logger.getLogger(JsonSchemaParser.class);
 
+	private String theSchemaRoot;
+
+	private java.util.Map<String, Object> theStoredSchemas;
+
+	/**
+	 * Creates a schema parser
+	 * 
+	 * @param schemaRoot The root URL to use to search for additional schemas
+	 */
+	public JsonSchemaParser(String schemaRoot)
+	{
+		theSchemaRoot = schemaRoot;
+		theStoredSchemas = new java.util.HashMap<String, Object>();
+	}
+
 	/**
 	 * Parses a JSON schema
 	 * 
@@ -26,34 +41,67 @@ public class JsonSchemaParser
 	 */
 	public JsonElement parseSchema(JsonElement parent, String name, Object schemaEl)
 	{
-		JsonElement ret;
+		JsonElement ret = createElementFor(schemaEl);
+		ret.configure(this, parent, name, schemaEl instanceof JSONObject ? (JSONObject) schemaEl
+			: null);
+		return ret;
+	}
+
+	/**
+	 * Creates an unconfigured JsonElement for the given schema object
+	 * 
+	 * @param schemaEl The schema object to create an element for
+	 * @return The JsonElement to parse the given schema type
+	 */
+	public JsonElement createElementFor(Object schemaEl)
+	{
 		if(schemaEl instanceof JSONObject)
 		{
 			JSONObject jsonSchema = (JSONObject) schemaEl;
 			String typeName = (String) jsonSchema.get("valueType");
 			if(typeName == null)
-				ret = new JsonObjectElement();
+				return new JsonObjectElement();
 			else if(typeName.equals("set"))
-				ret = new SetElement();
+				return new SetElement();
 			else if(typeName.equals("oneOf"))
-				ret = new EnumElement();
+				return new EnumElement();
 			else if(typeName.equals("string"))
-				ret = new StringElement();
+				return new StringElement();
 			else if(typeName.equals("boolean"))
 				return new BooleanElement();
 			else if(typeName.equals("number"))
 				return new NumberElement();
 			else
-				throw new IllegalStateException("Unrecognized valueType: " + typeName);
+				return createElementForType(typeName);
 		}
 		else if(schemaEl == null || schemaEl instanceof Boolean || schemaEl instanceof String
 			|| schemaEl instanceof Number)
-			ret = new ConstantElement(schemaEl);
+			return new ConstantElement(schemaEl);
 		else
 			throw new IllegalStateException("Unrecognized schema element type: "
 				+ schemaEl.getClass().getName());
-		ret.configure(this, parent, name, schemaEl instanceof JSONObject ? (JSONObject) schemaEl
-			: null);
-		return ret;
+	}
+
+	/**
+	 * @param type The type of schema to get
+	 * @return The schema for the given type
+	 */
+	public JsonElement createElementForType(String type)
+	{
+		Object ret = theStoredSchemas.get(type);
+		if(ret == null)
+		{
+			if(theSchemaRoot == null)
+				throw new IllegalStateException("No schema root to find schema type " + type);
+			try
+			{
+				ret = org.json.simple.JSONValue.parse(new java.io.InputStreamReader(
+					new java.net.URL(theSchemaRoot + type + ".json").openStream()));
+			} catch(java.io.IOException e)
+			{
+				throw new IllegalStateException("Could not find schema type " + type);
+			}
+		}
+		return createElementFor(ret);
 	}
 }
