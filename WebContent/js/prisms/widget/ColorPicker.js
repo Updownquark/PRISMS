5b2bdd076192d98242f504625e45c520062fcb88
Andrew Butler <Updownquark@gmail.com>
2010-01-19 21:54:56 +0000

    Improved the color widget and tested in firefox and IE

--- WebContent/js/prisms/widget/ColorPicker.js
+++ WebContent/js/prisms/widget/ColorPicker.js
@@ -1,405 +1,273 @@
+
 dojo.require("dijit.form.Slider");
 
-dojo.provide("prisms.widget.ColorPicker");
-dojo.declare("prisms.widget.ColorPicker", [dijit._Widget, dijit._Templated], {
+dojo.provide("iweda.ColorPicker");
+dojo.declare("iweda.ColorPicker", [dijit._Widget, dijit._Templated, dijit._Container], {
 
-	templatePath: "../view/prisms/templates/colorPicker.html",
+	templatePath: "/WeatherEffectsServlet/view/iweda/templates/colorPicker.html",
 
 	widgetsInTemplate: true,
 
-	displayAlpha: true,
+	_lastNotified: 0,
 
-	sqrt3: Math.sqrt(3),
+	alphaEnabled: false,
 
 	postCreate: function(){
 		this.inherited("postCreate", arguments);
-		this.setValue("#FFFFFF");
-		if(!this.displayAlpha)
-			PrismsUtils.setTableRowVisible(this.alphaRow, false);
-		var width=this.colorHexagon.width+"px";
-		var height=this.colorHexagon.height+"px";
-		this.colorShadeTable.style.width=width;
-		this.colorShadeTable.style.height=height;
-		this.colorTableDiv.style.height=height;
-		this.colorShade.style.height=height;
-	},
-
-	setVisible: function(visible){
-		if(dojo.isIE && visible)
-			this.domNode.style.display="block";
-		else if(visible)
-			this.domNode.style.display="table";
-		else
-			this.domNode.style.display="none";
-	},
-
-	isDisabled: function(){
-		return this.disabled;
-	},
-
-	setDisabled: function(disabled){
-		this.disabled=true;
-		this.redBox.disabled=disabled;
-		this.greenBox.disabled=disabled;
-		this.blueBox.disabled=disabled;
-		this.brightnessSlider.setAttribute("disabled", disabled);
-		this.alphaSlider.setAttribute("disabled", disabled);
-		this.alphaBox.disabled=disabled;
-	},
-
-	setValue: function(value, notifyUser){
-		if(typeof value=="string")
-		{
-			value={r: parseInt(value.substring(1, 3), 16),
-				g: parseInt(value.substring(3, 5), 16),
-				b: parseInt(value.substring(5, 7), 16)};
-		}
+		this.setAlphaEnabled(this.alphaEnabled);
 		this.dataLock=true;
 		try{
-			if(typeof value.a!="number")
-				value.a=255;
-			this.alphaSlider.setValue(value.a);
-			this.alphaBox.value=value.a;
-			this.redBox.value=value.r;
-			this.greenBox.value=value.g;
-			this.blueBox.value=value.b;
-			var brightness=this.getBrightness(value);
-			this.brightnessSlider.setValue(brightness);
-			var opacity=1-brightness/255;
-			this.colorShade.style.opacity=opacity;
-			if(this.colorShade.style.filters)
-				this.colorShade.style.filters.alpha.opacity=opacity*100;
-			this._positionMarker(value);
-			this._checkBrightColor(value);
+			this.setRGBValue(0, 0, 0, 0);
 		} finally{
 			this.dataLock=false;
 		}
-		this.value=value;
-		if(notifyUser)
-			this.onChange(value);
-	},
-	
-	getValue: function(){
-		return {r: this.value.r, g: this.value.g,
-			b: this.value.b, a: this.value.a};
 	},
 
-	getBrightness: function(value){
-		var ret=value.r;
-		if(value.g>ret)
-			ret=value.g;
-		if(value.b>ret)
-			ret=value.b;
+	setValue: function(value){
+		if(typeof value.r != "undefined")
+			this.setRGBValue(value.r, value.g, value.b, value.a);
+		else if(typeof value.red != "undefined")
+			this.setRGBValue(value.red, value.green, value.blue, value.alpha);
+		else if(typeof value.charAt == "function" && value.charAt(0)=='#')
+		{
+			var color=this.fromHtml(value);
+			this.setRGBValue(color.r, color.g, color.b, null);
+		}
+		else
+			throw new Error(dojo.toJson(color)+" is not a color");
+	},
+
+	setRGBValue: function(r, g, b, a){
+		this.dataLock=true;
+		try{
+			if(r!=null)
+			{
+				this.redSlider.setValue(r);
+				this.redSpinner.setValue(r);
+			}
+			if(g!=null)
+			{
+				this.greenSlider.setValue(g);
+				this.greenSpinner.setValue(g);
+			}
+			if(b!=null)
+			{
+				this.blueSlider.setValue(b);
+				this.blueSpinner.setValue(b);
+			}
+			if(this.alphaEnabled && a != null)
+			{
+				this.opacitySlider.setValue(a);
+				this.opacitySpinner.setValue(a);
+			}
+		} finally{
+			this.dataLock=false;
+		}
+		if(r!=null && g!=null && b!=null)
+			this.domNode.style.backgroundColor=this.toHtml(r, g, b);
+	},
+
+	getValue: function(){
+		var ret={r: this.redSpinner.getValue(),
+			g: this.greenSpinner.getValue(),
+			b: this.blueSpinner.getValue()};
+		if(this.alphaEnabled)
+			ret.a=this.opacitySpinner.getValue();
 		return ret;
 	},
 
-	_checkBrightColor: function(value){
-		if(!this._brightColor)
-		{
-			if(this.getBrightness(value)>10)
-				this._brightColor=this.getBrightColor(value);
-			else
-				this._brightColor={r: 255, g: 255, b: 255};
-			return;
-		}
-		var bc=this.getBrightColor(value);
-		if(!bc)
-			return;
-		if(bc.r!=this._brightColor.r || bc.g!=this._brightColor.g || bc.g!=this._brightColor.b)
-			this._brightColor=bc;
+	onChange: function(value){
 	},
 
-	getBrightColor: function(value){
-		var bright=this.getBrightness(value);
-		if(bright==255)
-			return {r: value.r, g: value.g, b: value.b};
-		if(bright==0)
-			return null;
-		return {r: Math.round(value.r/bright*255), g: Math.round(value.g/bright*255),
-			b: Math.round(value.b/bright*255)};
+	setEnabled: function(enabled){
+		this.redSlider.setAttribute("disabled", !enabled);
+		this.redSpinner.setAttribute("disabled", !enabled);
+		this.greenSlider.setAttribute("disabled", !enabled);
+		this.greenSpinner.setAttribute("disabled", !enabled);
+		this.blueSlider.setAttribute("disabled", !enabled);
+		this.blueSpinner.setAttribute("disabled", !enabled);
+		this.opacitySlider.setAttribute("disabled", !enabled);
+		this.opacitySpinner.setAttribute("disabled", !enabled);
 	},
 
-	_setRed: function(red){
-		var value=this.getValue();
-		value.r=red;
-		this.setValue(value, true);
+	setAlphaEnabled: function(alphaEnabled){
+		PrismsUtils.setTableRowVisible(this.opacityRow, alphaEnabled);
 	},
 
-	_setGreen: function(green){
-		var value=this.getValue();
-		value.g=green;
-		this.setValue(value, true);
-	},
-
-	_setBlue: function(blue){
-		var value=this.getValue();
-		value.g=green;
-		this.setValue(value, true);
-	},
-
-	_setAlpha: function(alpha){
-		var value=this.getValue();
-		value.a=alpha;
-		this.setValue(value, true);
-	},
-
-	_colorClicked: function(evt){
-		var x, y;
-		if(evt.clientX && evt.clientY){
-			x=evt.clientX;
-			y=evt.clientY;
-		} else if(evt.offsetX && evt.offsetY){
-			x=evt.offsetX;
-			y=evt.offsetY;
-		} else if(evt.layerX && evt.layerY){
-			x=evt.layerX-1;
-			y=evt.layerY-1;
-		} else {
-			x=evt.pageX-evt.target.offsetLeft;
-			y=evt.pageY-evt.target.offsetTop;
-		}
-		var coords=dojo.coords(this.colorShadeTable);
-		x-=coords.x;
-		y-=coords.y;
-
-		var sqrt3=this.sqrt3;
-		var sideLength=this.colorHexagon.width/2;
-		var _x=x;
-		x=sideLength*sqrt3/2-y;
-		y=sideLength-_x;
-		
-		var r, g, b;
-		if(x >= 0)
-		{
-			if(y >= x / sqrt3)
-			{
-				r = 1;
-				g = 1 - (y - x / sqrt3) / sideLength;
-				b = 1 - (y + x / sqrt3) / sideLength;
-			}
-			else
-			{
-				r = 1 - (x / sqrt3 - y) / sideLength;
-				g = 1;
-				b = 1 - 2 * x / sqrt3 / sideLength;
-			}
-		}
-		else
-		{
-			if(y >= -x / sqrt3)
-			{
-				r = 1;
-				g = 1 - (y - x / sqrt3) / sideLength;
-				b = 1 - (y + x / sqrt3) / sideLength;
-			}
-			else
-			{
-				r = 1 - (-y - x / sqrt3) / sideLength;
-				g = 1 + 2 * x / sqrt3 / sideLength;
-				b = 1;
-			}
-		}
-
-		if(r < 0 || r > 1 || g < 0 || g > 1 || b < 0 || b > 1)
-			return;
-
-		this.setValue({r: Math.round(r*255), g: Math.round(g*255), b: Math.round(b*255),
-			a: this.value.a}, true);
-	},
-
-	_positionMarker: function(value){
-		var bright=this.getBrightness(value);
-		if(bright>=128)
-			this.marker.style.borderColor="black";
-		else
-			this.marker.style.borderColor="white";
-		var sideLength=this.colorHexagon.width/2;
-		if(bright==0)
-		{
-			this.marker.style.left=sideLength+"px";
-			this.marker.style.top="0px";
-			return;
-		}
-		var r=value.r/bright;
-		var g=value.g/bright;
-		var b=value.b/bright;
-		var x, y;
-		var sqrt3=this.sqrt3;
-		if(r<0.999)
-		{
-			if(g<0.999)
-			{
-				x=(g-1)/2*sqrt3*sideLength;
-				y=(r-1)*sideLength-x/sqrt3;
-			}
-			else if(b<0.999)
-			{
-				x=(1-b)/2*sqrt3*sideLength;
-				y=(r-1)*sideLength+x/sqrt3;
-			}
-			else
-			{
-				x=0;
-				y=0;
-			}
-		}
-		else if(g<0.999 || b<0.999)
-		{
-			x=(g-b)*sideLength*sqrt3/2;
-			y=(2-g-b)*sideLength/2;
-		}
-		else
-		{
-			x=0;
-			y=0;
-		}
-		if(x<-sideLength*sqrt3/2 || x>sideLength*sqrt3/2 || y<-sideLength || y>sideLength)
-			return;
-		var _x=x;
-		x=sideLength-y;
-		y=sideLength*sqrt3/2-_x;
-		dojo.marginBox(this.marker, {t: Math.round(y)-2-this.colorHexagon.height, l:Math.round(x)-2});  
-	},
-
-	_isInteger: function(s){
-		return (s.toString().search(/^-?[0-9]+$/) == 0);
-	},
-
-	_redChanged: function(){
+	_redSliderChanged: function(){
 		if(this.dataLock)
-			return false;
-		var value=this.redBox.value;
-		var errorMsg=null;
-		if(isNaN(value))
-			errorMsg=value+" is not a valid red value--must be an integer between 0 and 255";
-		if(!this._isInteger(value))
-			errorMsg=value+" is not a valid red value--must be an integer between 0 and 255";
-		else if(value<0)
-			errorMsg=value+" is not a valid red value--must be an integer between 0 and 255";
-		else if(value>255)
-			errorMsg=value+" is not a valid red value--must be an integer between 0 and 255";
-		if(errorMsg)
-		{
-			this.error(errorMsg);
-			this.dataLock=true;
-			try{
-				this.redBox.value=this.value.r;
-			} finally{
-				this.dataLock=false;
-			}
-		}
-		this._setRed(value);
-	},
-
-	_greenChanged: function(){
-		if(this.dataLock)
-			return false;
-		var value=this.greenBox.value;
-		var errorMsg=null;
-		if(isNaN(value))
-			errorMsg=value+" is not a valid green value--must be an integer between 0 and 255";
-		if(!this._isInteger(value))
-			errorMsg=value+" is not a valid green value--must be an integer between 0 and 255";
-		else if(value<0)
-			errorMsg=value+" is not a valid green value--must be an integer between 0 and 255";
-		else if(value>255)
-			errorMsg=value+" is not a valid green value--must be an integer between 0 and 255";
-		if(errorMsg)
-		{
-			this.error(errorMsg);
-			this.dataLock=true;
-			try{
-				this.greenBox.value=this.value.g;
-			} finally{
-				this.dataLock=false;
-			}
-		}
-		this._setGreen(value);
-	},
-
-	_blueChanged: function(){
-		if(this.dataLock)
-			return false;
-		var value=this.blueBox.value;
-		var errorMsg=null;
-		if(isNaN(value))
-			errorMsg=value+" is not a valid blue value--must be an integer between 0 and 255";
-		if(!this._isInteger(value))
-			errorMsg=value+" is not a valid blue value--must be an integer between 0 and 255";
-		else if(value<0)
-			errorMsg=value+" is not a valid blue value--must be an integer between 0 and 255";
-		else if(value>255)
-			errorMsg=value+" is not a valid blue value--must be an integer between 0 and 255";
-		if(errorMsg)
-		{
-			this.error(errorMsg);
-			this.dataLock=true;
-			try{
-				this.blueBox.value=this.value.b;
-			} finally{
-				this.dataLock=false;
-			}
-		}
-		this._setBlue(value);
-	},
-
-	_brightnessChanged: function(){
-		if(this.dataLock)
-			return false;
-		var value={r: this._brightColor.r, g: this._brightColor.g, b: this._brightColor.b};
-		var newBright=this.brightnessSlider.getValue();
-		value.r=Math.round(value.r*newBright/255);
-		value.g=Math.round(value.g*newBright/255);
-		value.b=Math.round(value.b*newBright/255);
-		value.a=this.value.a;
-		this.setValue(value, true);
-	},
-
-	_alphaChangedB: function(){
-		if(this.dataLock)
-			return false;
-		var value=this.alphaBox.value;
-		var errorMsg=null;
-		if(isNaN(value))
-			errorMsg=value+" is not a valid alpha value--must be an integer between 0 and 255";
-		if(!this._isInteger(value))
-			errorMsg=value+" is not a valid alpha value--must be an integer between 0 and 255";
-		else if(value<0)
-			errorMsg=value+" is not a valid alpha value--must be an integer between 0 and 255";
-		else if(value>255)
-			errorMsg=value+" is not a valid alpha value--must be an integer between 0 and 255";
-		if(errorMsg)
-		{
-			this.error(errorMsg);
-			this.dataLock=true;
-			try{
-				this.alphaBox.value=this.value.a;
-			} finally{
-				this.dataLock=false;
-			}
-		}
+			return;
+		var r;
 		this.dataLock=true;
 		try{
-			this.alphaSlider.setValue(value);
+			r=this.redSlider.getValue();
+			r=Math.round(r);
+			this.redSlider.setValue(r);
+			this.redSpinner.setValue(r);
 		} finally{
 			this.dataLock=false;
 		}
-		this._setAlpha(value);
+		this._changed(r, null, null, null);
 	},
 
-	_alphaChangedS: function(){
+	_redSpinnerChanged: function(){
 		if(this.dataLock)
-			return false;
-		var value=this.alphaSlider.getValue();
+			return;
+		var r;
 		this.dataLock=true;
 		try{
-			this.alphaBox.value=value;
+			r=this.redSpinner.getValue();
+			r=Math.round(r);
+			this.redSlider.setValue(r);
+			this.redSpinner.setValue(r);
 		} finally{
 			this.dataLock=false;
 		}
-		this._setAlpha(value);
+		this._changed(r, null, null, null);
 	},
 
-	onChange: function(){
+	_greenSliderChanged: function(){
+		if(this.dataLock)
+			return;
+		var g;
+		this.dataLock=true;
+		try{
+			g=this.greenSlider.getValue();
+			g=Math.round(g);
+			this.greenSlider.setValue(g);
+			this.greenSpinner.setValue(g);
+		} finally{
+			this.dataLock=false;
+		}
+		this._changed(null, g, null, null);
+	},
+
+	_greenSpinnerChanged: function(){
+		if(this.dataLock)
+			return;
+		var g;
+		this.dataLock=true;
+		try{
+			g=this.greenSpinner.getValue();
+			g=Math.round(g);
+			this.greenSlider.setValue(g);
+			this.greenSpinner.setValue(g);
+		} finally{
+			this.dataLock=false;
+		}
+		this._changed(null, g, null, null);
+	},
+
+	_blueSliderChanged: function(){
+		if(this.dataLock)
+			return;
+		var b;
+		this.dataLock=true;
+		try{
+			b=this.blueSlider.getValue();
+			b=Math.round(b);
+			this.blueSlider.setValue(b);
+			this.blueSpinner.setValue(b);
+		} finally{
+			this.dataLock=false;
+		}
+		this._changed(null, null, b, null);
+	},
+
+	_blueSpinnerChanged: function(){
+		if(this.dataLock)
+			return;
+		var b;
+		this.dataLock=true;
+		try{
+			b=this.blueSpinner.getValue();
+			b=Math.round(b);
+			this.blueSlider.setValue(b);
+			this.blueSpinner.setValue(b);
+		} finally{
+			this.dataLock=false;
+		}
+		this._changed(null, null, b, null);
+	},
+
+	_opacitySliderChanged: function(){
+		if(this.dataLock)
+			return;
+		var a;
+		this.dataLock=true;
+		try{
+			a=this.opacitySlider.getValue();
+			a=Math.round(a);
+			this.opacitySlider.setValue(a);
+			this.opacitySpinner.setValue(a);
+		} finally{
+			this.dataLock=false;
+		}
+		this._changed(null, null, null, a);
+	},
+
+	_opacitySpinnerChanged: function(){
+		if(this.dataLock)
+			return;
+		var a;
+		this.dataLock=true;
+		try{
+			a=this.opacitySpinner.getValue();
+			a=Math.round(a);
+			this.opacitySlider.setValue(a);
+			this.opacitySpinner.setValue(a);
+		} finally{
+			this.dataLock=false;
+		}
+		this._changed(null, null, null, a);
+	},
+
+	_changed: function(r, g, b, a){
+		if(!r)
+			r=this.redSpinner.getValue();
+		if(!g)
+			g=this.greenSpinner.getValue();
+		if(!b)
+			b=this.blueSpinner.getValue();
+		if(!a && this.alphaEnabled)
+			a=this.opacitySpinner.getValue();
+		this.domNode.style.backgroundColor=this.toHtml(r, g, b);
+		if(this._onChangeCallback)
+		{
+			clearTimeout(this._onChangeCallback);
+			delete this._onChangeCallback;
+		}
+		var value={r: r, g: g, b: b}
+		var time=new Date().getTime();
+		if(this._lastNotified<time-1000)
+		{
+			this._lastNotified=time;
+			this.onChange(value);
+		}
+		else
+		{
+			var self=this;
+			this._onChangeCallback=setTimeout(function(){
+				delete self._onChangeCallback;
+				self._lastNotified=new Date().getTime();
+				self.onChange(value);
+			}, 1000);
+		}
+	},
+
+	fromHtml: function(color){
+		return {r: parseInt(color.substring(1, 3), 16),
+			g: parseInt(color.substring(3, 5), 16),
+			b: parseInt(color.substring(5, 7), 16)};
+	},
+
+	toHtml: function(r, g, b){
+		return "#"+this.toHex(r)+this.toHex(g)+this.toHex(b);
+	},
+
+	HEX_DIGS: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'],
+
+	toHex: function(value){
+		return this.HEX_DIGS[Math.floor(value/16)]+this.HEX_DIGS[value%16];
 	}
 });
--- src/prisms/ui/ColorHexagonGenerator.java
+++ src/prisms/ui/ColorHexagonGenerator.java
@@ -31,7 +31,7 @@ public class ColorHexagonGenerator
 
 		int getRGB(int x, int y)
 		{
-			x -= width / 2;
+			x -= width / 2 + 1;
 			y -= height / 2;
 			y = -y;
 
@@ -75,6 +75,100 @@ public class ColorHexagonGenerator
 			ret |= Math.round(b * 255);
 			return ret;
 		}
+
+		boolean shaded = true;
+
+		int getAlphaHex(int x, int y, int rgb)
+		{
+			if(shaded)
+				return getShadedHex(x, y, rgb);
+			else
+				return getCheckeredHex(x, y, rgb);
+		}
+
+		int mod = 64;
+
+		int getShadedHex(int x, int y, int rgb)
+		{
+			if(rgb == background)
+				return rgb;
+			int r = (rgb & 0xFF0000) >> 16;
+			int g = (rgb & 0xFF00) >> 8;
+			int b = rgb & 0xFF;
+
+			if(r == 255)
+			{
+				if(g == 255 || b == 255)
+					return 0xFF000000;
+				r = mod;
+				g = mod - ((g + 1) % mod);
+				b = mod - ((b + 1) % mod);
+				if(g >= mod / 2)
+					g = mod - g;
+				g *= 2;
+				if(b >= mod / 2)
+					b = mod - b;
+				b *= 2;
+			}
+			else if(g == 255)
+			{
+				if(b == 255)
+					return 0xFF000000;
+				g = mod;
+				r = mod - ((r + 1) % mod);
+				b = mod - ((b + 1) % mod);
+				if(r >= mod / 2)
+					r = mod - r;
+				r *= 2;
+				if(b >= mod / 2)
+					b = mod - b;
+				b *= 2;
+			}
+			else
+			{
+				b = mod;
+				r = mod - ((r + 1) % mod);
+				g = mod - ((g + 1) % mod);
+				if(r >= mod / 2)
+					r = mod - r;
+				r *= 2;
+				if(g >= mod / 2)
+					g = mod - g;
+				g *= 2;
+			}
+			float intens = r * 1.0f * g * b / (float) Math.pow(mod, 3);
+			intens = (float) Math.pow(intens, 0.5);
+			// intens = ((intens + 0.5f) * (intens + 0.5f) - .25f) / 2.25f;
+
+			int dark = Math.round(intens * 255);
+			return 0xFF000000 | (dark << 16) | (dark << 8) | dark;
+		}
+
+		int getCheckeredHex(int x, int y, int rgb)
+		{
+			if(rgb == background)
+				return rgb;
+			int r = (rgb & 0xFF0000) >> 16;
+			int g = (rgb & 0xFF00) >> 8;
+			int b = rgb & 0xFF;
+
+			if(r <= 4 || b <= 4 || g <= 4)
+				return 0xFF000000;
+
+			int bwg;
+			if(r == 255)
+				bwg = ((g / mod) + (b / mod)) % 3;
+			else if(g == 255)
+				bwg = ((r / mod) + (b / mod) + 1) % 3;
+			else
+				bwg = ((r / mod) + (g / mod) + 2) % 3;
+			if(bwg == 0)
+				return 0xFF000000;
+			else if(bwg == 1)
+				return 0xFFFFFFFF;
+			else
+				return 0xFF808080;
+		}
 	}
 
 	/**
@@ -84,32 +178,61 @@ public class ColorHexagonGenerator
 	 */
 	public static void main(String [] args)
 	{
-		generate(128, "C:\\Documents And Settings\\Andrew\\Desktop\\ColorHexagon.png");
+		generate(128, "C:\\Documents And Settings\\Andrew\\Desktop");
 	}
 
 	/**
-	 * Generates an RGB color hexagon and writes it to a PNG image file
+	 * Generates an RGB color hexagon, a plain black hexagon, and a black-and-white patterned
+	 * hexagon and writes them to PNG image files
 	 * 
 	 * @param dim The largest dimension of the image to generate
-	 * @param fileName The name of the file to write to
+	 * @param dirName The name of the directory to write the filesto
 	 */
-	public static void generate(int dim, String fileName)
+	public static void generate(int dim, String dirName)
 	{
+		if(dirName.charAt(dirName.length() - 1) != '/'
+			&& dirName.charAt(dirName.length() - 1) != '\\')
+			dirName += "/";
 		ColorMetadata md = new ColorMetadata(dim);
-		java.awt.image.BufferedImage im = new java.awt.image.BufferedImage(md.width, dim,
+		java.awt.image.BufferedImage colorImg = new java.awt.image.BufferedImage(md.height,
+			md.width, java.awt.image.BufferedImage.TYPE_INT_ARGB);
+		java.awt.image.BufferedImage blackImg = new java.awt.image.BufferedImage(md.height,
+			md.width, java.awt.image.BufferedImage.TYPE_INT_ARGB);
+		java.awt.image.BufferedImage bwImg = new java.awt.image.BufferedImage(md.height, md.width,
 			java.awt.image.BufferedImage.TYPE_INT_ARGB);
-		for(int y = 0; y < md.height; y++)
-			for(int x = 0; x < md.width; x++)
-				im.setRGB(x, y, md.getRGB(x, y));
+		for(int y = 0; y < md.width; y++)
+			for(int x = 0; x < md.height; x++)
+			{
+				int rgb = md.getRGB(md.width - y, x);
+				colorImg.setRGB(x, y, rgb);
+				if(rgb != md.background)
+					blackImg.setRGB(x, y, 0xFF000000);
+				else
+					blackImg.setRGB(x, y, rgb);
+				if(rgb == md.background)
+					bwImg.setRGB(x, y, rgb);
+				else
+				{
+					bwImg.setRGB(x, y, md.getAlphaHex(md.width - y, x, rgb));
+				}
+			}
 		try
 		{
-			java.io.FileOutputStream out = new java.io.FileOutputStream(fileName);
-			javax.imageio.ImageIO.write(im, "png", out);
-			out.flush();
+			java.io.FileOutputStream out = new java.io.FileOutputStream(dirName
+				+ "ColorHexagon.png");
+			javax.imageio.ImageIO.write(colorImg, "png", out);
+			out.close();
+
+			out = new java.io.FileOutputStream(dirName + "ShadeHexagon.png");
+			javax.imageio.ImageIO.write(blackImg, "png", out);
+			out.close();
+
+			out = new java.io.FileOutputStream(dirName + "AlphaHexagon.png");
+			javax.imageio.ImageIO.write(bwImg, "png", out);
 			out.close();
 		} catch(java.io.IOException e)
 		{
-			throw new IllegalStateException("Could not write image file " + fileName, e);
+			throw new IllegalStateException("Could not write image files to " + dirName, e);
 		}
 	}
 }
