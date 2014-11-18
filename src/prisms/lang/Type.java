82ba773625d05b15dd7603460d7092538a72e172
Andrew <abutler@psl.nmsu.edu>
2014-11-18 13:04:34 -0700

    Added Type.getWrapperType()

--- src/prisms/lang/Type.java
+++ src/prisms/lang/Type.java
@@ -681,8 +681,8 @@ public class Type {
 	}
 
 	/**
-	 * @param wrapper The wrapper for a primtive type
-	 * @return The wrapped primitive type, or null if the given type is not primitive or a primitive wrapper
+	 * @param wrapper The wrapper for a primitive type
+	 * @return The unwrapped primitive type, or null if the given type is not primitive or a primitive wrapper
 	 */
 	public static Class<?> getPrimitiveType(Class<?> wrapper) {
 		if(wrapper.isPrimitive())
@@ -703,6 +703,33 @@ public class Type {
 			return Byte.TYPE;
 		else if(wrapper.equals(Boolean.class))
 			return Boolean.TYPE;
+		else
+			return null;
+	}
+
+	/**
+	 * @param primitive The primitive type
+	 * @return The wrapped primitive type, or null if the given type is not primitive or a primitive wrapper
+	 */
+	public static Class<?> getWrapperType(Class<?> primitive) {
+		if(primitive.equals(Double.TYPE))
+			return Double.class;
+		else if(primitive.equals(Float.TYPE))
+			return Float.class;
+		else if(primitive.equals(Long.TYPE))
+			return Long.class;
+		else if(primitive.equals(Integer.TYPE))
+			return Integer.class;
+		else if(primitive.equals(Character.TYPE))
+			return Character.class;
+		else if(primitive.equals(Short.TYPE))
+			return Short.class;
+		else if(primitive.equals(Byte.TYPE))
+			return Byte.class;
+		else if(primitive.equals(Boolean.TYPE))
+			return Boolean.class;
+		else if(getPrimitiveType(primitive) != null)
+			return primitive;
 		else
 			return null;
 	}
